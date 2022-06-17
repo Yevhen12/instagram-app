60ce88bdb7e464581db26da3839f631a24aa1c52
Yevhen <epsonn537@gmail.com>
2022-06-18 00:36:51 +0300

    Searching system in development

--- src/components/SearchUsers.js
+++ src/components/SearchUsers.js
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
+import React, { useState, useRef } from "react";
 
 const SearchUsers = () => {
+
+    const inputEl = useRef(null);
     const [searchState, setSearchState] = useState(
         {
             searchFocus: false,
@@ -8,7 +10,7 @@ const SearchUsers = () => {
             seerchText: ''
         }
     )
-    
+
     const hendleFocus = () => {
         setSearchState(prevSearchState => {
             return (
@@ -16,12 +18,12 @@ const SearchUsers = () => {
                     ...prevSearchState,
                     searchFocus: true,
                     searchBlur: false
-    
+
                 }
             )
         })
     }
-    
+
     const hendleBlur = () => {
         setSearchState(prevSearchState => {
             return (
@@ -29,14 +31,14 @@ const SearchUsers = () => {
                     ...prevSearchState,
                     searchFocus: false,
                     searchBlur: true
-    
+
                 }
             )
         })
     }
-    
+
     const hendleChange = (e) => {
-        const {value} = e.target
+        const { value } = e.target
         setSearchState(prevSearchState => {
             return (
                 {
@@ -47,27 +49,59 @@ const SearchUsers = () => {
         })
     }
 
+    const clearInput = (e) => {
+        setSearchState(prevSearchState => {
+            return (
+                {
+                    ...prevSearchState,
+                    seerchText: '',
+                }
+            )
+        })
+        console.log(1)
+        inputEl.current.focus()
+    }
+
     const styleSeacrhBlur = {
-        backgroundImage: `url(../images/zoom-for-search.png)`,
-        backgroundSize: '14px 14px',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'top 12px left 15px',
-        opacity: "50%",
         paddingLeft: "40px",
     }
 
     console.log(searchState)
+
     return (
-        <div className="ml-10">
-        <input
-            placeholder="Search"
-            style = {!searchState.searchFocus ? styleSeacrhBlur : null}
-            className={`bg-[#8E8E8E]/20 outline-none py-1.5 px-4 rounded-lg placeholder:text-gray-700 placeholder:font-light w-[16.7rem] color-black`}
-            onFocus = {hendleFocus}
-            onBlur = {hendleBlur}
-            onChange = {hendleChange}
-        />
-    </div>
+        <div className="ml-10 relative">
+            <input
+                placeholder="Search"
+                style={!searchState.searchFocus ? styleSeacrhBlur : null}
+                className={`bg-[#8E8E8E]/20 outline-none py-1.5 px-4 rounded-lg placeholder:text-gray-400 placeholder:font-light w-[16.7rem]`}
+                onFocus={hendleFocus}
+                onBlur={hendleBlur}
+                onChange={hendleChange}
+                value = {searchState.seerchText}
+                ref={inputEl}
+            />
+            {
+                searchState.searchFocus ?
+                    (
+                        <>
+                            <div
+                                className="h-4 w-4 absolute top-2.5 right-4 opacity-20 cursor-pointer"
+                                onClick={() => console.log(1)}
+                            >
+                                <img src="/images/close-for-search.png"></img>
+                            </div>
+                        </>
+                    )
+                    :
+                    (
+                        <>
+                            <div className="h-3.5 w-3.5 absolute opacity-50 top-3 left-4">
+                                <img src="/images/zoom-for-search.png"></img>
+                            </div>
+                        </>
+                    )
+            }
+        </div>
     )
 }
 
